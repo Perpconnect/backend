5a0b651165173d998d9215387891776730dbcc1e
Shreyas Papinwar <spapinwar@gmail.com>
2021-10-09 21:26:58 +0530

    Added deployment to was lambda (#3)
    * Added mangum

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ from api.router import *
 from fastapi.middleware.cors import CORSMiddleware
 
 
-app = FastAPI(title="Perp connect - partial backend")
+app = FastAPI(title="Perp connect - partial backend", openapi_prefix="/main")
 
 app.add_middleware(
     CORSMiddleware,
